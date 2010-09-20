77556d1959cc33c6f3b0669fa908c72ec732b1f9
Steve Smith <github@scsworld.co.uk>
2010-09-20 21:28:36 +0100

    add verify signature method to validate the signature sent by the upcoming server update

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,8 +1,23 @@
 class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :verify_signature
+  
+  SECRET = 'f811674dbd7be5b5e77f'
   
   def create
     message = Mail.new(params[:message])
     render :text => message.subject
   end
+  
+  protected
+  
+    def verify_signature
+      provided = request.request_parameters.delete(:signature)
+      signature = Digest::MD5.hexdigest(request.request_parameters.sort.map{|k,v| v}.join + SECRET)
+      
+      if provided != signature
+        render :text => "Message signature fail #{provided} != #{signature}", :status => 403
+        return false 
+      end
+    end
 end
