deb6c52441c12bf3b371f275e32714e468599d93
Steve Smith <github@scsworld.co.uk>
2011-03-24 13:35:35 +0000

    add ability to flatten params so that attachments will correctly validate with the signature

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -13,11 +13,25 @@ class IncomingMailsController < ApplicationController
   
     def verify_signature
       provided = request.request_parameters.delete(:signature)
-      signature = Digest::MD5.hexdigest(request.request_parameters.sort.map{|k,v| v}.join + SECRET)
+      signature = Digest::MD5.hexdigest(flatten_params(request.request_parameters).sort.map{|k,v| v}.join + SECRET)
       
       if provided != signature
         render :text => "Message signature fail #{provided} != #{signature}", :status => 403, :content_type => Mime::TEXT.to_s
-        return false 
+        return false
       end
+    end
+    
+    def flatten_params(params, title = nil, result = {})
+      params.each do |key, value|
+        if value.kind_of?(Hash)
+          key_name = title ? "#{title}[#{key}]" : key
+          flatten_params(value, key_name, result)
+        else
+          key_name = title ? "#{title}[#{key}]" : key
+          result[key_name] = value
+        end
+      end
+    
+      return result
     end
 end
